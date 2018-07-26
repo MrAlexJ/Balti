0a5386d7fd29005730b07435fd909d4a52c64101
MrAlexJ <acjimenez86@gmail.com>
2018-07-26 15:01:14 -0700

    fixedmarks

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Login from "./components/Login.js";
 import Home from "./components/Home.js";
 import axios from "axios";
 //import logo from "./logo.svg";
 //import "./App.css";
-=======
 import "./App.css";
 import Profile from "./components/pages/Profile"
->>>>>>> b108828aac0a9cf94b6505da3d23e08452394cd2
 
 class App extends Component {
   state = {
