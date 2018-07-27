2412fe04b1f922deaa0d9eccd867d49ce01293bb
MrAlexJ <acjimenez86@gmail.com>
2018-07-27 13:14:57 -0700

    loginjs

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,57 +1,56 @@
-import React, {Component} from "react";
-import axios from "axios";
 
-class Login extends Component{
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
     state = {
-        username: "",
-        password: "",
-        error: null
+        email: "",
+        password: ""
     };
 
-    handleInputChange = (event) =>{
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+
         this.setState({
-            [event.target.name]: event.target.value
+            [name]: value
         });
     };
 
-    handleFormSubmit = (event) => {
+    handleLogIn = (event) => {
         event.preventDefault();
-        axios.post("/login", this.state).then((res) =>{
-            if(res.data === true){
-                this.props.setLogin();
-                this.props.history.push("/");
-            }
-            else {
-                this.setState({
-                    error: "Failed to log in"
-                })
+
+        let existingUser = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        console.log("CURRENT USER: ", existingUser);
+
+        axios.post('/login', existingUser).then(response => {
+            if (response.data.code === 504) {
+                alert("Invalid login.");;
+                window.location.href='/login'
+            } else {
+                alert("You have been logged in!");
+                window.location.href='/';
             }
         });
-    };
+    }
 
-    render(){
-        return(
-            <form>
-                <input
-                value={this.state.username}
-                name="username"
-                onChange={this.handleInputChange}
-                type="text"
-                placeholder="Username"
-                />
-                <input
-                value={this.state.password}
-                name="password"
-                onChange={this.handleInputChange}
-                type="password"
-                placeholder="Password"
-                />
-
-                <button onClick={this.handleFormSubmit}>Submit</button>
-
-                <span>{this.state.error}</span>
+    render() {
+        return (
+            <form className="form" onSubmit={this.handleLogIn}>
+                <div className="form-group">
+                    <input value={this.state.value} name="email" onChange={this.handleInputChange} type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                </div>
+                <div className="form-group">
+                    <input value={this.state.value} name="password" onChange={this.handleInputChange} type="password" className="form-control" id="password" placeholder="Password"></input>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">Log in</button>
+                </div>
             </form>
-        );
+        )
     }
 }
 
